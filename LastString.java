d2697deccd769d701a85e541686883d5ec985f7b
chandu11215 <kalalshannu19@gmail.com>
2020-06-22 18:47:37 +0530

    string can only store text values

---
+++ LastString.java
@@ -0,0 +1,11 @@
+public class LastString{
+  public static void main(String[] args){
+
+     String lastString = "10";
+      int myInt =50;
+     lastString = lastString + myInt;
+     System.out.println("LastString is equal to " + lastString);
+   }
+}
+
+
